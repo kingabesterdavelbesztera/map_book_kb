561c5b54da8dc0e08d1c9df41760fc8ca14b986d
kingabesterdavelbesztera <163434972+kingabesterdavelbesztera@users.noreply.github.com>
2024-04-16 18:23:24 +0200

    wip: add new menu function, add user append

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from models.data_source import users
-from package.crud import read_friends
+from package.crud import read_friends, add_user
 
 
 
@@ -8,8 +8,11 @@ if __name__ == "__main__":
         print("Welcome to the menu choose an option: ")
         print("1. Read a list of friend")
         print("0. Exit")
+        print("2. add new users")
         menu_option = input("Choose an option:")
         if menu_option == "0":
             break
         if menu_option == "1":
             read_friends(users)
+        if menu_option == "2":
+            add_user(users)
