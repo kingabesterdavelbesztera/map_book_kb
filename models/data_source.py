67541625b02259863a2913af017bbaf7aafa4441
kingabesterdavelbesztera <163434972+kingabesterdavelbesztera@users.noreply.github.com>
2024-04-16 17:48:44 +0200

    wip: code refactor add menu

---
+++ models/data_source.py
@@ -0,0 +1,5 @@
+users: list = [
+
+    {"name": "Julia", "surname": "Gotowiec", "posts": 1500, },
+    {"name": "Hubert", "surname": "Sybilski", "posts": 123456, },
+    {"name": "Adrian", "surname": "Dobrzański", "posts": 3, }]
