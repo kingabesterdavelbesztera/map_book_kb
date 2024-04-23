67aed92d6b78f4158a9010933775e15b2e88de25
kingabesterdavelbesztera <163434972+kingabesterdavelbesztera@users.noreply.github.com>
2024-04-23 19:24:53 +0200

    feat: add an interactive map

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from models.data_source import users
 from package.crud import read_friends, add_user, search_user, remove_user, update_user
+from package.emap import single_map, full_map
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     while True:
-        print("Welcome to the menu choose an option: ")
-        print("1. Read a list of friend")
-        print("0. Exit")
-        print("2. add new users")
-        print("3. Search")
-        print("4 remove")
+        print("Welcome to the menu  ")
+        print("0. Exit ")
+        print("1. Read a list of friends ")
+        print("2. Add new user")
+        print("3. Search user")
+        print("4. Remove user")
         print("5. Update user")
+        print("6. Generate map")
+        print("7. Generate full map")
         menu_option = input("Choose an option:")
         if menu_option == "0":
             break
@@ -23,3 +26,7 @@ if __name__ == "__main__":
             remove_user(users)
         if menu_option == "5":
             update_user(users)
+        if menu_option == "6":
+            single_map(search_user(users)['location'])
+        if menu_option == "7":
+            full_map(users)
